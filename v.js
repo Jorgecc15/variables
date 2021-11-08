bb439ac68ef421d0ea1bbb13cd3d1bfbbbb2ea17
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-11-08 17:10:07 -0500

    Delete v.js

--- v.js
+++
@@ -1,7 +0,0 @@
-var numero = prompt("ingrese un numero")
-var numero2 = prompt("Ingrese otro numero")
-var suma = numero + numero2
-alert("La multiplicación de los dos números es "+( numero * numero2 ))
-alert("La resta de los dos números es " + (numero - numero2 ))
-alert("La división de los dos números es "+(numero / numero2))
-alert("La suma de os dos números es " + suma)
